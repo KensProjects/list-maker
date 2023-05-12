7cc5e006815b585cca08c30eab50afe450bf1166
KensProjects <kdwilbusiness@gmail.com>
2023-05-12 03:44:44 -0400

    modified:   server/src/controllers/userControllers.ts

--- server/src/controllers/userControllers.ts
+++ server/src/controllers/userControllers.ts
@@ -66,22 +66,23 @@ export async function registerUser(req: IRequest, res: Response) {
     const user = await User.findOne({ username: username });
     if (user) return res.status(403).json({ message: "Registration failed!" });
     const protectedPass = await bcrypt.hash(password, 15);
-    const createdUser = await User.create({
-      username: username,
-      password: protectedPass,
-      list: [],
-    });
-    const payload = {
-      id: createdUser._id,
-      username: createdUser.username,
-      password: createdUser.password,
-    };
+    return res.json({message: protectedPass, username, password})
+    // const createdUser = await User.create({
+    //   username: username,
+    //   password: protectedPass,
+    //   list: [],
+    // });
+    // const payload = {
+    //   id: createdUser._id,
+    //   username: createdUser.username,
+    //   password: createdUser.password,
+    // };
 
-    const token = jwt.sign(payload, process.env.SECRET as string, {
-      expiresIn: "1h",
-    });
-    const hr = 1000 * 60 * 60; //1 hour
-    res.json({token:token})
+    // const token = jwt.sign(payload, process.env.SECRET as string, {
+    //   expiresIn: "1h",
+    // });
+    // const hr = 1000 * 60 * 60; //1 hour
+    // res.json({token:token})
 
     //  return res
     //   .cookie("token", token, {
